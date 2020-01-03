32659adeb8c05b698562e2807c601fd171461d47
msilva99 <a83840@alunos.uminho.pt>
2020-01-03 19:31:06 +0000

    imagens

--- Não-Relacional/neo4j/export_csv.sql
+++ Não-Relacional/neo4j/export_csv.sql
@@ -8,7 +8,8 @@ FROM Artigo JOIN Filme ON Filme.id_artigo = Artigo.id_artigo;
 SELECT Artigo.id_artigo, titulo, tipo, preco, ano, classificacao, autor, genero, editora, n_paginas
 FROM Artigo JOIN Livro ON Livro.id_artigo = Artigo.id_artigo;
 
-SELECT Artigo.id_artigo, titulo, tipo, preco, ano, classificacao, plataforma, idade_min, publisher, n_jogadores_max,genero
+SELECT Artigo.id_artigo, titulo, tipo, preco, ano, classificacao, plataforma, idade_min,
+publisher, n_jogadores_max, genero
 FROM Artigo JOIN Jogo ON Jogo.id_artigo = Artigo.id_artigo;
 
 SELECT Artigo.id_artigo, titulo, tipo, preco, ano, classificacao, genero_musical, artista, formato
